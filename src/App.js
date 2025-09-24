5e18aa7c107cc1fb7879cd200817079e9766cf17
t-hn13 <nguyentanhoanghkl13200801@gmail.com>
2025-09-24 14:55:07 +0700

    Cập nhật lần nữa  App.js

--- src/App.js
+++ src/App.js
@@ -14,9 +14,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello World, Lại là Hoàng đây</p>
         <a
           className="App-link"
           href="https://reactjs.org"
